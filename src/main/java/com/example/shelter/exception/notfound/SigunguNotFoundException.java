446766f7e99582f7c71bbea7549a035c0f6efbb6
mingu <alsrn7590@naver.com>
2023-06-29 15:56:40 +0900

    feat : SigunguNotFoundException 의 생성자들 추가

--- src/main/java/com/example/shelter/exception/notfound/SigunguNotFoundException.java
+++ src/main/java/com/example/shelter/exception/notfound/SigunguNotFoundException.java
@@ -1,8 +1,19 @@
 package com.example.shelter.exception.notfound;
 
+import java.util.Map;
+
 public class SigunguNotFoundException extends ResourceNotFoundException{
 
     public SigunguNotFoundException() {
         super("101", "존재하지 않는 시/군/구입니다.");
     }
+
+    public SigunguNotFoundException(Long id) {
+        super("101", "존재하지 않는 시/군/구입니다.", Map.of("id", id.toString()));
+    }
+
+    public SigunguNotFoundException(String name) {
+        super("101", "존재하지 않는 시/군/구입니다.", Map.of("name", name));
+    }
+
 }
