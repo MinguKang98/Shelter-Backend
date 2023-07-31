ce2b943ad69102bdc73cb655ddacff17a793c261
mingu <alsrn7590@naver.com>
2023-07-31 21:56:34 +0900

    feat : CommonExceptionHandler 의 예외처리 추가

--- src/main/java/com/example/shelter/exception/CommonExceptionHandler.java
+++ src/main/java/com/example/shelter/exception/CommonExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.shelter.exception;
 
+import com.example.shelter.exception.badinput.BadInputException;
+import com.example.shelter.exception.duplicate.DuplicateException;
 import com.example.shelter.exception.notfound.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +22,36 @@ public class CommonExceptionHandler {
                 .errors(Map.of())
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<ErrorResponse> DuplicateExceptionHandler(DuplicateException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .errors(Map.of())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadInputException.class)
+    public ResponseEntity<ErrorResponse> BadInputExceptionHandler(BadInputException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .errors(Map.of())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> ConstraintViolationExceptionHandler(ConstraintViolationException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("000")
+                .message(e.getMessage())
+                .errors(Map.of())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
