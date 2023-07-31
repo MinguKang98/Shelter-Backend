3b59906df18174ad77fb8190f71569ef9ab8ec5e
mingu <alsrn7590@naver.com>
2023-07-31 21:56:08 +0900

    feat : TsunamiShelterDto, ShelterPageDto, ShelterListDto 생성

---
+++ src/main/java/com/example/shelter/common/dto/ShelterListDto.java
@@ -0,0 +1,37 @@
+package com.example.shelter.common.dto;
+
+import com.example.shelter.shelter.Shelter;
+import com.example.shelter.tsunamishelter.TsunamiShelter;
+import com.example.shelter.tsunamishelter.dto.TsunamiShelterDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+public class ShelterListDto {
+
+    private long totalCount;
+
+    private List<?> content;
+
+    protected ShelterListDto(List<? extends Shelter> content) {
+        this.totalCount = content.size();
+        this.content = content.stream()
+                .map(s ->
+                        switch (s.getShelterType()) {
+                            case TSUNAMI -> TsunamiShelterDto.of((TsunamiShelter) s);
+                            case EARTHQUAKE -> null;
+                            case CIVIL_DEFENCE -> null;
+                        }
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static ShelterListDto of(List<? extends Shelter> content) {
+        return new ShelterListDto(content);
+    }
+
+}
---
+++ src/main/java/com/example/shelter/common/dto/ShelterPageDto.java
@@ -0,0 +1,53 @@
+package com.example.shelter.common.dto;
+
+import com.example.shelter.shelter.Shelter;
+import com.example.shelter.tsunamishelter.TsunamiShelter;
+import com.example.shelter.tsunamishelter.dto.TsunamiShelterDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+public class ShelterPageDto {
+
+    private long totalCount;
+
+    private int totalPage;
+
+    private int page;
+
+    private int size;
+
+    boolean hasNext;
+
+    boolean hasPrevious;
+
+    private List<?> content;
+
+    protected ShelterPageDto(Page<? extends Shelter> page) {
+        this.totalCount = page.getTotalElements();
+        this.totalPage = page.getTotalPages();
+        this.page = page.getNumber() + 1;
+        this.size = page.getNumberOfElements();
+        this.hasNext = page.hasNext();
+        this.hasPrevious = page.hasPrevious();
+        this.content = page.getContent().stream()
+                .map(s ->
+                        switch (s.getShelterType()) {
+                            case TSUNAMI -> TsunamiShelterDto.of((TsunamiShelter) s);
+                            case EARTHQUAKE -> null;
+                            case CIVIL_DEFENCE -> null;
+                        }
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static ShelterPageDto of(Page<? extends Shelter> page) {
+        return new ShelterPageDto(page);
+    }
+
+}
