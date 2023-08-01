c056aa6cc7a06e4e09186d4ff6250e0676ffab6f
mingu <alsrn7590@naver.com>
2023-08-01 20:48:50 +0900

    feat : ShelterVolumeDto, SearchVolumeService 생성

---
+++ src/main/java/com/example/shelter/seachvolume/dto/ShelterVolumeDto.java
@@ -0,0 +1,18 @@
+package com.example.shelter.seachvolume.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ShelterVolumeDto {
+
+    private long tsunami;
+
+    private long earthquake;
+    
+    private long civilDefense;
+
+}
