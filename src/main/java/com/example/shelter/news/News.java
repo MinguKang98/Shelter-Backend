8994c28a8d283fb07e4737d5d0b157f3c8bc81b3
mingu <alsrn7590@naver.com>
2023-08-06 01:42:53 +0900

    feat : News, NewsRepository 생성

---
+++ src/main/java/com/example/shelter/news/News.java
@@ -0,0 +1,50 @@
+package com.example.shelter.news;
+
+import com.example.shelter.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "news")
+@NoArgsConstructor
+public class News extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
+    private Long id;
+
+    @Column
+    private LocalDateTime publishTime;
+
+    @Column
+    private String writer;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private boolean isDeleted = false;
+
+    @Builder
+    public News(Long id, LocalDateTime publishTime, String writer, String title, String content) {
+        this.id = id;
+        this.publishTime = publishTime;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updateDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+}
---
+++ src/main/java/com/example/shelter/news/repository/NewsRepository.java
@@ -0,0 +1,38 @@
+package com.example.shelter.news.repository;
+
+import com.example.shelter.news.News;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+    @Query("select n from News n where n.id = :id and n.isDeleted = false")
+    Optional<News> findByIdNotDeleted(@Param("id") Long id);
+
+    @Query("select n from News n where n.isDeleted = false ")
+    Page<News> findAllNotDeleted(Pageable pageable);
+
+    @Query("""
+            select n from News n
+            where n.id < :id and
+            n.isDeleted = false
+            order by n.id desc
+            """)
+    List<News> findPrevByIdNotDeleted(@Param("id") Long id, Pageable pageable);
+
+
+    @Query("""
+            select n from News n
+            where n.id > :id and
+            n.isDeleted = false
+            order by n.id asc
+            """)
+    List<News> findNextByIdNotDeleted(@Param("id") Long id, Pageable pageable);
+
+}
