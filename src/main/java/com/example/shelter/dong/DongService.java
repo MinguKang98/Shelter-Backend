9a260d33cbd3e57bcb08a923b6fe5894a3695fca
mingu <alsrn7590@naver.com>
2023-09-27 18:22:17 +0900

    feat : DongService 에 findByCurrent 추가

--- src/main/java/com/example/shelter/dong/DongService.java
+++ src/main/java/com/example/shelter/dong/DongService.java
@@ -1,7 +1,9 @@
 package com.example.shelter.dong;
 
 import com.example.shelter.exception.notfound.DongNotFoundException;
+import com.example.shelter.shelter.address.Address;
 import com.example.shelter.sigungu.Sigungu;
+import com.example.shelter.util.NaverApiParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.util.List;
 public class DongService {
 
     private final DongRepository dongRepository;
+    private final NaverApiParser naverApiParser;
 
     public List<Dong> findAll() {
         return dongRepository.findAllNotDeleted(Sort.by(Sort.Direction.ASC, "name"));
@@ -57,6 +60,17 @@ public class DongService {
         Dong dong = dongRepository.findByIdNotDeleted(id)
                 .orElseThrow(() -> new DongNotFoundException(id));
         dong.updateDeleted(true);
+    }
+
+    public Dong findByCurrent(double curLat, double curLon) {
+        Address address = naverApiParser.getAddressByCurrent(curLat, curLon);
+        Dong dong = dongRepository.findByAddressNames(
+                address.getSidoName(),
+                address.getSigunguName(),
+                address.getDongName()
+        ).orElseThrow(()-> new DongNotFoundException(address.getFullDongName()));
+
+        return dong;
     }
 
 }
